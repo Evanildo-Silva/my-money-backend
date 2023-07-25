bfa3d99d5296c5a3974a38a0ec5aba12c0a1ba54
Evanildo Silva <evanildo.sf91@gmail.com>
2023-07-25 17:04:25 -0300

    feat: count

--- src/api/biilingCycle/billingCycleService.js
+++ src/api/biilingCycle/billingCycleService.js
@@ -3,19 +3,31 @@ const BillingCycle = require('./billingCycle')
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
 BillingCycle.updateOptions({ new: true, runValidators: true })
 
+// buscando o contador
+BillingCycle.route('count', (req, res, next) => {
+  BillingCycle.count((error, value) => {
+    // tratando os erros
+    if (error) {
+      res.status(500).json({ errors: [error] })
+    } else {
+      res.json({ value })
+    }
+  })
+})
+
 BillingCycle.route('get', (req, res, next) => {
 
   BillingCycle.find({}, (err, docs) => {
 
-      if(!err) {
+    if (!err) {
 
-          res.json(docs)
+      res.json(docs)
 
-      } else {
+    } else {
 
-          res.status(500).json({errors: [error]})
+      res.status(500).json({ errors: [error] })
 
-      }
+    }
 
   })
 
